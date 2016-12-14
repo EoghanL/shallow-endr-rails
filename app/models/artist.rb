bbfaecc7707ba09ea3dea9be97e13b507fb260ff
github username <github email address>
2016-12-14 15:19:34 -0500

    further validations for database sanitization

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,6 @@ class Artist < ApplicationRecord
   has_many :songs
   has_many :users_artists
   has_many :artists, through: :users_artists
+
+  validates_uniqueness_of :mb_id
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :artist
   has_many :rankings
+  validates_uniqueness_of :mb_id
 end
