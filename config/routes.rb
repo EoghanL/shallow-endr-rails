9974515f87f2a2baa673d57b5e440a1761687f38
marcimmediato <marc.immediato@gmail.com>
2016-12-20 19:28:19 -0500

    split to new repo, setting up for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   resources :rankings
   resources :users_artists
   resources :future_songs
+
+  root 'welcome#index'
+
   get '/login', to: 'sessions#new'
   get 'users/getUserId', to: 'users#getUserId'
   post '/sessions/login', to: 'sessions#login'
   post '/users/signup', to: 'users#create'
   post '/users/addSong', to: 'users#add_song'
   post '/future_songs/:id', to: 'future_songs#destroy'
-  # get '/callback', to: 'users#callback'
   get '/search', to: 'artists#search'
   post '/rankings/delete/:id', to: 'rankings#destroy'
   resources :songs
