6b3107bcad156f16cf8de13292bbc3d7e7b8c009
marcimmediato <marc.immediato@gmail.com>
2017-01-06 11:14:42 -0500

    fixing sign up.  users are now logged in once they register successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      jwt_token = Auth.issue({user_id: @user.id})
+      render json: { user: @user, jwt: jwt_token }
     else
       render json: @user.errors, status: :unprocessable_entity
     end
